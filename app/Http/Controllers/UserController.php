afeba224f28543720414bb1e8642d85aa3c61fb9
nguyenmanhhung280901 <nguyenmanhhung280901@gmail.com>
2021-06-24 11:30:27 +0700

    Upload project php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(){
+        return view('user.register');
+    }
+
+    function xuliRegister(Request $request){
+        $fullName = $request->get('fullname');
+        $email = $request->get('email');
+        $phone = $request->get('phone');
+        $address = $request->get('address');
+        $identityCard = $request->get('identityCard');
+        return sprintf('Your information is fullname: %s, email: %s,  phone: %s, address: %s, identityCard: %s', $fullName, $email, $phone, $address, $identityCard);
+    }
+
+    public function login(Request $request){
+        $fullName = $request->get('fullname');
+        $email = $request->get('email');
+        $phone = $request->get('phone');
+        $address = $request->get('address');
+        $identityCard = $request->get('identityCard');
+        return sprintf('Welcome to admin fullname %s, email %s,  phone %s, address %s, identityCard %s', $fullName, $email, $phone, $address, $identityCard);
+    }
+
+    public function getUserDetail($id){
+        return 'hello path' . $id;
+    }
+}
